988a269ab762e674180b496936b7f369561e947d
s1mple d3v <gamerdevfe@gmail.com>
2020-04-25 00:59:41 -0300

    Finish

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +23,22 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // pegou tudo no corpo
+    const { title, value, type } = request.body;
+    // cuida da regra de negócio
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    /**
+     * Executar o método execute que irá executar a transação e
+     * retornar a transação que será criada
+     */
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
